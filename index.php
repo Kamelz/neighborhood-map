ad26c540a39dcd57630fa9c0ce86527c70be1072
Kamelz <Mohamed.Kamel@student.aast.edu>
2018-03-19 07:02:44 +0200

    Add title

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="css/simple-sidebar.css"/>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <title>Neighborhood Map</title>
   </head>
   <body>
     <div id="wrapper" class="toggled">
